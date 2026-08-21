b9c078280563d4af50b6e01c53140f1dbdfa55b8
WPEssential <wordpress.essential@gmail.com>
2026-08-21 05:20:41 +0500

    Register Forms service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\FormsServiceProvider;
 use App\Providers\NexoraServiceProvider;
 
 return [
     AppServiceProvider::class,
+    FormsServiceProvider::class,
     NexoraServiceProvider::class,
 ];
