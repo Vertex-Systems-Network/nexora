e46c97cfa0422450b4bf03b80257e5564c234d49
WPEssential <wordpress.essential@gmail.com>
2026-08-22 02:50:12 +0500

    N1.25: require Backup DR Upgrade in development readiness

--- scripts/development-readiness.php
+++ scripts/development-readiness.php
@@ -72,6 +72,7 @@ $run('forge_developer_experience_product_contract', 'Forge / Developer Experienc
 $run('sentinel2_product_contract', 'Sentinel 2.0 product source contract', [PHP_BINARY, 'scripts/sentinel2-product-contract-verify.php']);
 $run('marketplace2_product_contract', 'Marketplace 2.0 product source contract', [PHP_BINARY, 'scripts/marketplace2-product-contract-verify.php']);
 $run('cloud_ha_product_contract', 'Cloud / HA product source contract', [PHP_BINARY, 'scripts/cloud-ha-product-contract-verify.php']);
+$run('backup_dr_upgrade_product_contract', 'Backup / DR / Upgrade product source contract', [PHP_BINARY, 'scripts/backup-dr-upgrade-product-contract-verify.php']);
 
 $vendorReady = is_file($root.'/vendor/autoload.php');
 $nodeReady = is_dir($root.'/node_modules') && (is_file($root.'/node_modules/typescript/bin/tsc') || is_file($root.'/node_modules/.bin/tsc'));
