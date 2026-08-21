f62a29210ee1abe363c02848e07d062a733f1f68
WPEssential <wordpress.essential@gmail.com>
2026-08-21 12:28:40 +0500

    chore: register marketplace service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,12 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\FormsServiceProvider;
+use App\Providers\MarketplaceServiceProvider;
 use App\Providers\NexoraServiceProvider;
 
 return [
     AppServiceProvider::class,
     FormsServiceProvider::class,
+    MarketplaceServiceProvider::class,
     NexoraServiceProvider::class,
 ];
