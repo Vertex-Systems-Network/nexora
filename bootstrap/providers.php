486db73793f04f2fd15110d123d349651285efa8
WPEssential <wordpress.essential@gmail.com>
2026-08-21 15:01:06 +0500

    Register Commerce service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\CommerceServiceProvider;
 use App\Providers\FormsServiceProvider;
 use App\Providers\MarketplaceServiceProvider;
 use App\Providers\NexoraServiceProvider;
 
 return [
     AppServiceProvider::class,
+    CommerceServiceProvider::class,
     FormsServiceProvider::class,
     MarketplaceServiceProvider::class,
     NexoraServiceProvider::class,
